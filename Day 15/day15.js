735a99a33b6ce927b00c996ad33534d444dab920
Ben Tom Abey <127616692+EDBOZY@users.noreply.github.com>
2023-10-26 19:32:45 +0400

    Add files via upload

---
+++ Day 15/day15.js
@@ -0,0 +1,19 @@
+const cts=document.querySelectorAll('.counter')
+cts.forEach(counter=>{
+	counter.innerText='0'
+	const update=()=>{
+		const target=+counter.getAttribute('data-target')
+		const c=+counter.innerText
+
+		const increment=target/200
+		 if(c<target){
+		 	counter.innerText=`${Math.ceil(c+increment)}`
+		 	setTimeout(update,1)
+		 }
+		 else{
+		 	counter.innerText=target
+		 }
+	}
+	update()
+	
+})
